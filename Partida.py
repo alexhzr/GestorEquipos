90e0cfb15bbbe3e7d8b73857fa62a864c2259144
Álex Hernández <alex.hdz94@gmail.com>
2018-11-08 15:54:51 +0100

    Añadidas funcionalidades a la clase Partida

--- Partida.py
+++ Partida.py
@@ -1,12 +1,24 @@
-
+import datetime
 
 class Partida:
+    lugar = ""
+    fecha = -1
+    
     def __init__(self, lugar, fecha, hora):
         self.lugar = lugar
-        self.fecha = fecha
-        self.hora = hora
+
+        fechaSeparada = fecha.split("/")
+        dia = int(fechaSeparada[0])
+        mes = int(fechaSeparada[1])
+        year = int(fechaSeparada[2])
+
+        horaSeparada = hora.split(":")
+        hora = int(horaSeparada[0])
+        minutos = int(horaSeparada[1])
+
+        self.fecha = datetime.datetime(year, mes, dia, hora, minutos)
 
     def detalles(self):
         print("Lugar: " + self.lugar)
-        print("Fecha: " + self.fecha + " " + self.hora)
+        print("Fecha: " + self.fecha.strftime("%d/%m/%y %H:%M"))
         print("------------------------")
