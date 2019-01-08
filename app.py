7eab0a38e973047fed449be9e7d2bc9c718c63c4
Álex Hernández <alex.hdz94@gmail.com>
2019-01-08 23:29:05 +0100

    Añadidas funcionalidades a la API. Creadas rutas para hacerla funcional. Closes #8.

--- app.py
+++ app.py
@@ -1,47 +1,263 @@
 from flask import Flask, render_template, request, session, json
 from mongoengine import *
-import Agenda
-from Partida import *
+
+from .modelos import Agenda, Equipo, Jugador, Partida
+from .constantes import *
+
 app = Flask(__name__)
 
 connect('heroku_8jrhk4q8', host='mongodb://user:user1234@ds117334.mlab.com:17334')
 
-@app.route('/')
-def status():
-    status = {
-      "status": "OK",
-      "ejemplo": {
-        "ruta": "/listar",
-        "valor": "{ 'respuesta':'vacio' }"
-      }
-    }
-
+def jsonear(respuesta):
     response = app.response_class(
-        response=json.dumps(status),
+        response=json.dumps(respuesta),
         status=200,
         mimetype='application/json'
     )
 
     return response
 
-"""@app.route('/listar', methods=['POST'])
-def listarPartidas():
-    equipo = request.form['equipo']
+@app.route('/borrar_datos')
+def borrar_datos():
+    Equipo.objects.delete()
+    Agenda.objects.delete()
 
-@app.route('/crear_agenda', methods=['POST'])
-def crearAgenda():
-    equipo = request.form['equipo']
+    respuesta = {
+        "status": OK, "mensaje": "Base de datos limpiada"
+    }
 
-    if equipo in db:
-        status = {
-            "status": "ERROR",
-            "mensaje": "El equipo ya existe"
-          }
+    return jsonear(respuesta)
+
+@app.route('/inicializar_datos')
+def inicializar_datos():
+    if Equipo.objects().count() != 0:
+        borrar_datos()
+        respuesta = {
+            "status": "FAILED",
+            "reason": "Ya está inicializada la BD con datos de prueba."
         }
 
-        response = app.response_class(
-            response=json.dumps(status),
-            status=200,
-            mimetype='application/json'
-        )
-    else:"""
+    else:
+        agenda = Agenda()
+
+        datos = {
+            "equipos" : [
+                { "Papaya FC": [
+                    { "nombre": "Jason Osuna", "edad": 23, "nick": "Jason" },
+                    { "nombre": "Wisin Romeldo", "edad": 21, "nick": "Wisin" },
+                    { "nombre": "Welintong Quiw", "edad": 20, "nick": "El Campeón" }
+                ]},
+
+                { "Zurriagazo FC": [
+                    { "nombre": "Bernardo Gado", "edad": 26, "nick": "Berni" },
+                    { "nombre": "Horacio Fameldas", "edad": 23, "nick": "Horace" },
+                    { "nombre": "Euladio Contreras", "edad": 19, "nick": "El Contreras" }
+                ]},
+
+                { "Átomos de Springfield": [
+                    { "nombre": "Homer Simpson", "edad": 41, "nick": "Da Homa" },
+                    { "nombre": "Ned Flanders", "edad": 54, "nick": "Flanders" },
+                    { "nombre": "Moe Swislak", "edad": 42, "nick": "Moé" }
+                ]} ,
+
+                { "Dummy Crash Dolls": [
+                    { "nombre": "Moñeco 1", "edad": 1, "nick": "Moñeco1" },
+                    { "nombre": "Moñeco 2", "edad": 1, "nick": "Moñeco2" },
+                    { "nombre": "Moñeco 3", "edad": 1, "nick": "Moñeco3" }
+                ]}
+            ],
+            "partidos": [
+                {   "local": "Papaya FC",
+                    "visitante": "Zurriagazo FC",
+                    "lugar": "Estadio Santa Juana",
+                    "fecha": "10/01/2019",
+                    "hora": "12:00"
+                },
+                {   "local": "Dummy Crash Dolls",
+                    "visitante": "Átomos de Springfield",
+                    "lugar": "Estadio de Prueba 01",
+                    "fecha": "15/01/2019",
+                    "hora": "18:00"
+                },
+                {   "local": "Átomos de Springfield",
+                    "visitante": "Papaya FC",
+                    "lugar": "Estadio Duff de Springfield",
+                    "fecha": "20/01/2019",
+                    "hora": "21:00"
+                },
+                {   "local": "Zurriagazo FC",
+                    "visitante": "Papaya FC",
+                    "lugar": "Estadio Santa Euladia",
+                    "fecha": "25/01/2019",
+                    "hora": "12:00"
+                }
+            ]
+        }
+
+        keys = [ "Papaya FC", "Zurriagazo FC", "Átomos de Springfield", "Dummy Crash Dolls" ]
+        i = 0
+
+        for e in datos['equipos']:
+            equipo = Equipo(nombre=keys[i])
+
+            for j in datos['equipos'][i][keys[i]]:
+                jugador = Jugador(nombre=j['nombre'], edad=j['edad'], nick=j['nick'])
+                equipo.add_jugador(jugador)
+
+            equipo.save()
+            i += 1
+
+        for p in datos['partidos']:
+            local = p['local']
+            visitante = p['visitante']
+            lugar = p['lugar']
+            fecha = p['fecha']
+            hora = p['hora']
+
+            i += 1
+
+            if agenda.crear_partida(local, visitante, lugar, fecha, hora) == ERROR:
+                respuesta = { "status": ERROR, "mensaje": "Fallo creando partida" }
+
+                return jsonear(respuesta)
+
+            agenda.save()
+
+        respuesta = {
+            "status": OK, "mensaje": "Insertados datos de prueba"
+        }
+
+    return jsonear(respuesta)
+
+
+@app.route('/')
+def status():
+    status = {
+      "status": OK,
+      "ejemplo": {
+        "ruta": "/equipo/listar",
+        "valor": "{ 'respuesta':'vacio' }"
+      }
+    }
+
+    return jsonear(status)
+
+"""
+############################ RUTAS PARA EQUIPO ############################
+"""
+@app.route('/equipo', methods=['GET'])
+def listar_equipos():
+    equipos = []
+    if Equipo.objects().count() != 0:
+        for equipo in Equipo.objects:
+            equipos.append(json.loads(equipo.to_json()))
+
+        respuesta = { "status" : OK, "equipos" : equipos }
+
+    else:
+        respuesta = { "status": OK, "equipos" : 0 }
+
+    return jsonear(respuesta)
+
+
+@app.route('/equipo', methods=['POST'])
+def crearEquipo():
+    nombre_equipo = request.args.get('nombre')
+    equipo = Equipo.objects(nombre=nombre_equipo)
+
+    if equipo:
+        respuesta = {
+            "status": "FAILED",
+            "error": "Ya existe ese equipo"
+        }
+
+    else:
+        equipo = Equipo(nombre=nombre_equipo)
+        equipo.save()
+
+        respuesta = {
+            "status": OK,
+            "id_equipo": str(equipo.id)
+        }
+
+    return jsonear(respuesta)
+
+
+"""
+############################ RUTAS PARA JUGADOR ############################
+"""
+@app.route('/jugador', methods=['POST'])
+def crearJugador():
+    id_equipo = request.args.get("id_equipo")
+    equipo = Equipo.objects(id=id_equipo)
+
+    if not equipo:
+        respuesta = {
+            "status": "FAILED",
+            "error": "Ese equipo no existe en la BD"
+        }
+
+    else:
+        nombre_jugador = request.args.get('nombre')
+        equipo = equipo.first()
+
+        if equipo.tiene_jugador(nombre_jugador):
+            respuesta = {
+                "status": "FAILED",
+                "error": "Ya existe ese jugador"
+            }
+
+        else:
+            jugador = Jugador(nombre=nombre_jugador)
+            jugador.edad = request.args.get('edad')
+            jugador.nick = request.args.get('nick')
+
+            equipo.add_jugador(jugador)
+            equipo.save()
+
+            respuesta = {
+                "status": OK,
+                "jugador_creado": json.loads(jugador.to_json()),
+                "equipo": json.loads(equipo.to_json())
+            }
+
+    return jsonear(respuesta)
+
+"""
+############################ RUTAS PARA PARTIDA ############################
+"""
+
+@app.route('/partida', methods=['POST'])
+def crear_partida():
+    idLocal = request.args.get("local")
+    idVisitante = request.args.get("visitante")
+    fecha = request.args.get("fecha")
+    hora = request.args.get("hora")
+    lugar = request.args.get("lugar")
+
+    local = Equipo.objects(id=idLocal)
+    visitante = Equipo.objects(id=idVisitante)
+
+    if not local or not visitante:
+        respuesta = {
+            "status": "FAILED",
+            "error": "Ese equipo no existe en la BD"
+        }
+
+    elif local and visitante:
+        agenda = Agenda.objects.first()
+        local = local.first()
+        visitante = visitante.first()
+
+        if agenda.crear_partida(local.id, visitante.id, lugar, fecha, hora) == OK:
+            respuesta = {
+                "status": OK,
+                "mensaje": "Creada partida correctamente para la partida de " + fecha + " " + hora + " en " + lugar
+            }
+        else:
+            respuesta = {
+                "status": ERROR,
+                "mensaje": "Error creando la partida"
+            }
+
+    return jsonear(respuesta)
--- test.py
+++ test.py
@@ -1,17 +1,38 @@
 # coding=utf-8
-import Partida
-from Agenda import Agenda
+from .modelos import Agenda, Equipo, Jugador, Partida
+from .app import *
+import re
 
 
-agenda = Agenda(nombre="Villazarcillo FC").save()
+def test_datos_prueba():
+    borrar_datos()
+    inicializar_datos()
+    assert Equipo.objects.count() == 4, "No se ha creado bien"
+    assert len(Equipo.objects().get(nombre="Zurriagazo FC").jugadores) == 3, "No se han insertado todos los jugadores del ZFC"
+    assert len(Equipo.objects().get(nombre="Papaya FC").jugadores) == 3, "No se han insertado todos los jugadores del Papaya"
+    assert len(Equipo.objects().get(nombre="Átomos de Springfield").jugadores) == 3, "No se han insertado todos los jugadores del Átomos"
+    assert len(Equipo.objects().get(nombre="Dummy Crash Dolls").jugadores) == 3, "No se han insertado todos los jugadores del DCT"
 
-def test_crearAgenda():
-    assert agenda.nombre != "", "La agenda no se ha creado bien"
+    assert len(Agenda.objects()) == 1, "No se ha inicializado bien la agenda"
+    assert len(Agenda.objects[0].partidas) == 4, "No se han inicializado bien las partidas"
 
-def test_crearPartida():
-    agenda.crearPartida("Montejicar", "12/02/2020", "20:30")
-    assert agenda.partidas[0].lugar != "", "La partida no se ha creado bien"
-    assert agenda.partidas[0].fecha != -1, "La partida no se ha creado bien"
+def test_crear_jugador():
+    equipo = Equipo("Testing Team FC")
+    assert len(equipo.jugadores) == 1, "No se crean bien los jugadores"
+    
+def test_crear_partida():
+    agenda = Agenda.objects().first()
+    assert agenda.crear_partida("Zurriagazo FC", "Átomos de Springfield", "Estadio Nuevo Méjico", "01/02/2020 20:30") == 1, "Error creando partida"
 
-def test_contarPartidas():
-    assert agenda.contarPartidas() != 0, "La cuenta de partiads está mal"
+def test_contar_partidas():
+    agenda = Agenda.objects().first()
+    assert agenda.contar_partidas() == 5, "La cuenta de partidas está mal"
+    assert agenda.contar_partidas(mes=1) == 4, "La cuenta por meses está mal"
+    assert agenda.contar_partidas(year=2019) == 4, "La cuenta por años está mal"
+    assert agenda.contar_partidas(mes=2) == 1, "La cuenta (febrero) está mal"
+    assert agenda.contar_partidas(year=2020) == 1, "La cuenta (2020) está mal"
+
+def test_formatear_fecha():
+    agenda = Agenda.objects().first()
+    patt = re.compile("^(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}\s+(0[0-9]|1[0-9]|2[0-3])\:(0[0-9]|[1-5][0-9])$")
+    assert patt.match(agenda.partidas[0].get_fecha()) is not None, "La fecha no sigue el formato establecido"
